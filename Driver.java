eac1e8647adedafe5f3dd1b66e9a9374b241337a
brewerkm1 <101234022+brewerkm1@users.noreply.github.com>
2022-04-14 09:18:57 -0400

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -3,10 +3,18 @@ public class Driver
     Player player = new Player();
     Enemy enemy = new Enemy();
 
-    public static void main(String[] args)
-    {
-
+    public static void main(String[] args) {
+        Player player;
+        Enemy enemy;
+        intro();
     }
+
+    public static void intro(){
+        Scanner kb = new Scanner(System.in);
+        System.out.println("A strange, ornate letter sits atop your lopsided, peeling table. Very strange, considering you live" +
+                "alone. ...And you locked the door before you left this morning. You should read it." +
+                "\n'TO WHOM IT MAY CONCERN," +
+                "\n[come back]");
 
     public void battle(Player player, Enemy enemy)
     {
