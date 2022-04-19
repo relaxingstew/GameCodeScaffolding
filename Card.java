ae10a45c65efcbe9a001d56de0ccc112ee6a94aa
relaxingstew <86404787+relaxingstew@users.noreply.github.com>
2022-04-19 10:57:01 -0400

    Update Card.java

--- Card.java
+++ Card.java
@@ -1,16 +1,21 @@
 public class Card extends Deck
 {
+    private String name;
     private int strength;
     private int defense;
 
-    Card(int strength, int defense)
+    Card(String name, int strength, int defense)
     {
+        this.name = name;
         this.strength = strength;
         this.defense = defense;
     }
 
-
     //Getters and setters
+
+    public String getName() {
+        return name;
+    }
 
     public int getStrength() {
         return strength;
