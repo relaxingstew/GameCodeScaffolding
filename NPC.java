10504be46253d9dc8d38eb5f185b115d30d61b83
brewerkm1 <101234022+brewerkm1@users.noreply.github.com>
2022-04-25 19:19:11 -0400

    Update NPC.java

--- NPC.java
+++ NPC.java
@@ -1,6 +1,7 @@
+import java.util.Random;
 import java.util.Scanner;
 
-public class NPC
+public class NPC extends Player
 {
     private boolean npcAlignment;
     private int npcStrength;
@@ -10,6 +11,7 @@ public class NPC
         int input;
         System.out.println("Villager approaches! Villager says: Hi, traveller.");
         System.out.println("\n1. What can you tell me about this area?" +
+                //not this!!!
                 "\n2. Up for a trade?" +
                 "\n3. [Ignore]");
         input = kb.nextInt();
@@ -17,6 +19,8 @@ public class NPC
             System.out.println("");
         }
         if(input == 2){
+            Random rand = new Random();
+
             System.out.println("");
         }
         if(input == 3){
@@ -24,44 +28,88 @@ public class NPC
         }
     }
 
-    public static int badNPCInteraction(int currentCurrency){
+    public static void badNPCInteraction(){
         Scanner kb = new Scanner(System.in);
         int input;
         System.out.println("Villager approaches! Villager says: Got any cash?");
-        System.out.println("1. [Give 1 coin]");
-        System.out.println("2. No, sorry.");
-        System.out.println("3. [Ignore]");
+        System.out.println("""
+
+                1. [Give 1 coin]
+                2. No, sorry.
+                3. [Ignore]""");
         input = kb.nextInt();
         if(input == 1){
             System.out.println("You lost 1 coin.)");
-            currentCurrency = currentCurrency -1;
             System.out.println("Villager: That's funny. Got any more?");
-            System.out.println("1. No.");
-            System.out.println("2. [Give 1 coin]");
+            System.out.println("""
+
+                    1. No.
+                    2. [Give 1 coin]""");
             input = kb.nextInt();
             if(input == 1){
                 System.out.println("Villager: Well, I got your wallet right here, and you got plenty.");
                 System.out.println("The villager robbed you! Money lost...");
-                currentCurrency = currentCurrency - 50;
+                //add amount of money later
             }
             if(input == 2){
                 System.out.println("(You lost 1 coin.)");
-                currentCurrency = currentCurrency -1;
                 System.out.println("Villager: Yeah, all right. Very, VERY funny.");
                 System.out.println("The villager left, grumbling.");
             }
         }
         if(input == 2){
             System.out.println("Villager: You sure?");
+            System.out.println("""
+
+                1. I'm on an important mission.
+                2. VERY sure.
+                3. [Ignore]""");
+            input = kb.nextInt();
+            if(input == 1){
+                System.out.println("Villager: Doesn't sound important. You're no fun.");
+                System.out.println("The villager left, bored.");
+            }
+            if(input == 2){
+                System.out.println("Villager: Well, I got your wallet right here, and you got plenty.");
+                System.out.println("The villager robbed you! Money lost...");
+                //add amount of money later
+            }
+            if(input == 3){
+                System.out.println("Fine. Be that way.");
+                System.out.println("The villager left, grumbling.");
+            }
         }
         if(input == 3){
             System.out.println("Villager: Hey! I was talking to you!");
-            System.out.println("1. No money. Sorry.");
-            System.out.println("2. [Ignore]");
+            System.out.println("""
+
+                    1. No money. Sorry.
+                    2. [Ignore]""");
             input = kb.nextInt();
+            if(input == 1){
+                System.out.println("Villager: Are you that hero? That one supposed to save us all? You don't look like much.");
+                System.out.println("""
+
+                    1. Yeah. Maybe.
+                    2. [Ignore]""");
+                    input = kb.nextInt();
+                    if(input == 1){
+                        System.out.println("Villager: Some hero you are.");
+                        System.out.println("Villager left, disappointed.");
+                    }
+                    if(input == 2) {
+                        System.out.println("Villager: Some hero you are.");
+                        System.out.println("Villager left, disappointed.");
+                    }
+            }
+            if(input == 2){
+                System.out.println("Villager: I'll get you one day. Just wait and see.");
+                System.out.println("Villager left, angry.");
+            }
         }
-        return currentCurrency;
     }
+
+
     //Getters and setters
 
     public boolean getNPCAlignment() {
@@ -73,7 +121,13 @@ public class NPC
     }
 
     public void setNpcAlignment(boolean npcAlignment) {
-        this.npcAlignment = npcAlignment;
+        Random align = new Random();
+        this.npcAlignment = align.nextBoolean();
+        getNPCAlignment();
+        if(npcAlignment == true){
+            goodNPCInteraction();
+        }
+        else badNPCInteraction();
     }
 
     public void setNpcStrength(int npcStrength) {
